d744ead219a8fb29b8dee024bf7ab3e770e6343b
Ahsan Majid Bhatti <ahsanmajidbhatti49@gmail.com>
2021-08-21 18:23:21 +0500

    Secure_Asset Added

--- resources/views/backend/layouts/footer.blade.php
+++ resources/views/backend/layouts/footer.blade.php
@@ -3,6 +3,15 @@
         <b class="copyright">&copy; 2014 Edmin - EGrappler.com </b>All rights reserved.
     </div>
 </div>
+@if(parse_url(url('/'), PHP_URL_SCHEME) == 'HTTPS')
+<script src="{{ secure_asset('edmin/code.scripts/jquery-1.9.1.min.js') }}" type="text/javascript"></script>
+<script src="{{ secure_asset('edmin/code.scripts/jquery-ui-1.10.1.custom.min.js') }}" type="text/javascript"></script>
+<script src="{{ secure_asset('edmin/code.bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
+<script src="{{ secure_asset('edmin/code.scripts/flot/jquery.flot.js') }}" type="text/javascript"></script>
+<script src="{{ secure_asset('edmin/code.scripts/flot/jquery.flot.resize.js') }}" type="text/javascript"></script>
+<script src="{{ secure_asset('edmin/code.scripts/datatables/jquery.dataTables.js') }}" type="text/javascript"></script>
+<script src="{{ secure_asset('edmin/code.scripts/common.js') }}" type="text/javascript"></script>
+@else
 <script src="{{ asset('edmin/code.scripts/jquery-1.9.1.min.js') }}" type="text/javascript"></script>
 <script src="{{ asset('edmin/code.scripts/jquery-ui-1.10.1.custom.min.js') }}" type="text/javascript"></script>
 <script src="{{ asset('edmin/code.bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
@@ -10,5 +19,6 @@
 <script src="{{ asset('edmin/code.scripts/flot/jquery.flot.resize.js') }}" type="text/javascript"></script>
 <script src="{{ asset('edmin/code.scripts/datatables/jquery.dataTables.js') }}" type="text/javascript"></script>
 <script src="{{ asset('edmin/code.scripts/common.js') }}" type="text/javascript"></script>
+@endif
 
 </body>
